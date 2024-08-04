12a83ccd08a9e4c882ed9e33a9bb62867388960d
Jonathan Ko <39708300+jonathanko45@users.noreply.github.com>
2024-08-04 01:18:10 -0400

    Update SerDes_TB.sv

--- SerDes_TB.sv
+++ SerDes_TB.sv
@@ -2,9 +2,7 @@
 time scale: https://www.chipverify.com/verilog/verilog-timescale
 
 send a ton of data all at once.
-send the data as decimal (convert it here, or don't)
-eg send 11001001 01010101 10100011 01011101  all at once (32 bit)
-32'b11001001010101011010001101011101
+eg send  01011101  all at once (8 bits)
 
 the serializer will then take this and split it up, sending it with the clock
 
@@ -13,52 +11,127 @@ pass in the DEPTH as well relative to the size of the text
 
 if we want 1Gigabit/sec 
 with bus width of 1 (sending bits 1 at a time) need 1024 MHz to get 1 GB/s
+
+lets do 
+slow = 20 MHz 
+fast = 1000 MHz
 */
 
 /* CURRENT STEPS
-1. figure out how to split string into bytes
-	https://www.chipverify.com/systemverilog/systemverilog-packed-arrays
-2. figure out how the scheme works for numbers like 11111
-	need to use case statements for the 'lookup table'
-3. figure out how to get the disparity (sum the digits?)
-4. do the disparity change
-5. figure out how to reassemble into serialized value (should be easy)
-6. do 3b/4b scheme case statement
-7. RD needs to be done properly (not twice with both 5b and 3b)
+Serializer
+2. add resets
 
 
-8. make clock speed
-9. use 2 flip-flops to speed up to faster clock domain
-10. send to output
-Serializer done
 
 Deserializer
 1. make FIFO https://vlsiverify.com/verilog/verilog-codes/asynchronous-fifo/
-2. 
+2. edit FIFO to have almost full and change to use that (maybe)
+3. serial to parallel convert (slow clock does this)
+4. decode the data 
+5. output to TB
 
 */
 
 module SerDes_Project_TB();
-  reg r_Clk;
-  reg [31:0] r_Data;
+  
+  parameter DATA_WIDTH = 8;
+  
+  //for Serializer
+  reg r_Clk, r_Clk_Fast;
+  reg [DATA_WIDTH-1:0] r_Data;
   reg signed [1:0] r_RD = 2'sb11; //RD = -1
-  wire [31:0] w_Ser_Data;
+  wire [DATA_WIDTH-1:0] w_Ser_Data;
   
-  
-  SerDes_Project UUT
+  Serializer seUUT
   (.i_Clk(r_Clk),
+   .i_Clk_Fast(r_Clk_Fast),
    .i_Data(r_Data),
    .i_RD(r_RD),
-   .o_Ser_Data(w_Ser_Data)
-  );
+   .o_Ser_Data(w_Ser_Data));
   
-  always #1 r_Clk <= !r_Clk;
+  //for Deserializer
+  reg r_W_en, r_Wrst_n;
+  reg r_R_en, r_Rrst_n;
+  reg [DATA_WIDTH-1:0] r_Data_In;
+  wire [DATA_WIDTH-1:0] w_Data_Out;
+  wire w_full, w_empty;
+
+  // Queue to push data_in (not synthesizable, only for simulation)
+  reg [DATA_WIDTH-1:0] r_Wdata_q[$], r_Wdata;
   
-  initial begin
+  Deserializer deUUT
+  (.i_Rclk(r_Clk),
+   .i_Wclk(r_Clk_Fast),
+   .i_Wrst_n(r_Wrst_n),
+   .i_Rrst_n(r_Rrst_n),
+   .i_W_en(r_W_en),
+   .i_R_en(r_R_en),
+   .i_Data_In(r_Data_In),
+   .o_Data_Out(w_Data_Out),
+   .o_full(w_full),
+   .o_empty(w_empty));
+   
+  always #50ns r_Clk = ~r_Clk; //20MHz read
+  always #1ns r_Clk_Fast = ~r_Clk_Fast; //1000MHz write
+  
+  /*
+  initial begin //fast (write) clock
+    r_Clk_Fast = 1'b0;
+    r_Wrst_n = 1'b0;
+    r_W_en = 1'b0;
+    r_Data_In = 0;
+    
+   
+    repeat(10) @(posedge r_Clk_Fast);
+    r_Wrst_n = 1'b1;
+    
+    repeat (2) begin
+      for (int i=0; i<30; i++) begin
+        @(posedge r_Clk_Fast iff !w_full); //if and only iff !w_full
+        r_W_en = (i%2 == 0)? 1'b1 : 1'b0; //alternates?
+        if(r_W_en) begin
+          r_Data_In = $urandom; //randomizer
+          r_Wdata_q.push_back(r_Data_In);
+        end
+      end
+      #50;
+    end
+  end
+  */
+  
+  
+  initial begin //slow read clock
     $dumpfile("dump.vcd");
     $dumpvars;
     
-    r_Data <= 32'b11001001010101011010001101011101;
+    /*
+    r_Clk = 1'b0;
+    r_Rrst_n = 1'b0;
+    r_R_en = 1'b0;
+    
+    repeat(20) @(posedge r_Clk);
+    r_Rrst_n = 1'b1;
+    
+    repeat(2) begin
+      for (int i=0; i<30; i++) begin
+        @(posedge r_Clk iff !w_empty);
+        r_R_en = (i%2 == 0)? 1'b1 : 1'b0;
+        if(r_R_en) begin
+          r_Wdata = r_Wdata_q.pop_front();
+          if (w_Data_Out !== r_Wdata) 
+            $error ("Time = %0t: Comparison FAILED: expected wr_data = %h, rd_data = %h", $time, r_Wdata, w_Data_Out);
+          else
+            $display ("Time = %0t: Comparison PASSED: wr_data = %h, rd_data = %h", $time, r_Wdata, w_Data_Out);
+        end
+      end
+      #50;
+    end
+    */
+      
+    
+    
+    //r_Data <= 32'b11001001010101011010001101011101;
+    r_Data <= 8'b01011101;
     #10;
     $display("Sending: %b", r_Data);
     $display("8b %b", r_Data[7:0]);
@@ -68,9 +141,8 @@ module SerDes_Project_TB();
     
     //if (w_Ser_Data == r_Data[7:0]) 
     $display ("TEST  ##### %b", w_Ser_Data);
-   
-      
-   
+    
+    
 
     $finish();
   end
